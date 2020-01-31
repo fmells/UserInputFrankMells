42488c737f52c1d396c9dd8af25d74b6555c27a6
Frank Mells <fmells@mail.usf.edu>
2020-01-31 17:57:19 -0500

    Add project files.

---
+++ UserInputFrankMells/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserInputFrankMells
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey there! This is Frank the French Fry! What's your favorite food?");
+            Console.WriteLine("Type in the answer and press the ENTER button!");
+            string favoriteFood;
+            favoriteFood = Console.ReadLine();
+            Console.WriteLine("Your favorite food is " + favoriteFood +"? Delicious!");
+        }
+    }
+}
